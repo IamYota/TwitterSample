ff3108144d29edc521d333b87efb3810d4233a32
Yota Yamashita <yota.yamashita@gmail.com>
2023-10-25 15:42:42 +0900

    UITableViewの追加

---
+++ TwitterSample/TweetDataModel.swift
@@ -0,0 +1,14 @@
+//
+//  TweetDataModel.swift
+//  TwitterSample
+//
+//  Created by Yota Yamashita on 2023/10/25.
+//
+
+import Foundation
+
+struct TweetDataModel {
+    var userName: String
+    var text: String
+    var recordDate: Date
+}
